88d7580abcb0089ff43afcfaa2850946ccdf34f5
Julius Rio Shol <2021154969@ub.edu.bz>
2023-02-15 12:43:01 -0600

    second

--- main.go
+++ main.go
@@ -8,31 +8,6 @@ import (
 	"strings"
 )
 
-// func one(wg *sync.WaitGroup) {
-// 	defer wg.Done() //Done is decrementing , defer - is allways  the last that get run within a function
-// 	fmt.Println("hola")
-// }
-// func two(wg *sync.WaitGroup) {
-// 	defer wg.Done() //Done is decrementing , defer - is allways  the last that get run within a function
-// 	fmt.Println("ni hao")
-// }
-
-// func three(wg *sync.WaitGroup) {
-// 	defer wg.Done() //Done is decrementing , defer - is allways  the last that get run within a function
-// 	fmt.Println("hello")
-// }
-// func main() {
-
-// 	var wg sync.WaitGroup
-// 	wg.Add(3)
-// 	go one(&wg)
-// 	go two(&wg)
-// 	go three(&wg)
-
-// 	// let's delay
-// 	wg.Wait() //Wait means: wait until wg holds the value 0.
-// }
-
 func main() {
 	//set the flags:
 	msg := flag.String("msg", "Howdy Stranger!", "The message to display")
