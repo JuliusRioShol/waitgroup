dc8b05136521dafbd7800424860cf7ce7b3b4403
Julius Rio Shol <2021154969@ub.edu.bz>
2023-02-15 12:42:10 -0600

    First Commit

--- main.go
+++ main.go
@@ -3,31 +3,55 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"sync"
+	"strings"
 )
 
-func one(wg *sync.WaitGroup) {
-	defer wg.Done() //Done is decrementing , defer - is allways  the last that get run within a function
-	fmt.Println("hola")
-}
-func two(wg *sync.WaitGroup) {
-	defer wg.Done() //Done is decrementing , defer - is allways  the last that get run within a function
-	fmt.Println("ni hao")
-}
+// func one(wg *sync.WaitGroup) {
+// 	defer wg.Done() //Done is decrementing , defer - is allways  the last that get run within a function
+// 	fmt.Println("hola")
+// }
+// func two(wg *sync.WaitGroup) {
+// 	defer wg.Done() //Done is decrementing , defer - is allways  the last that get run within a function
+// 	fmt.Println("ni hao")
+// }
 
-func three(wg *sync.WaitGroup) {
-	defer wg.Done() //Done is decrementing , defer - is allways  the last that get run within a function
-	fmt.Println("hello")
-}
+// func three(wg *sync.WaitGroup) {
+// 	defer wg.Done() //Done is decrementing , defer - is allways  the last that get run within a function
+// 	fmt.Println("hello")
+// }
+// func main() {
+
+// 	var wg sync.WaitGroup
+// 	wg.Add(3)
+// 	go one(&wg)
+// 	go two(&wg)
+// 	go three(&wg)
+
+// 	// let's delay
+// 	wg.Wait() //Wait means: wait until wg holds the value 0.
+// }
+
 func main() {
+	//set the flags:
+	msg := flag.String("msg", "Howdy Stranger!", "The message to display")
+	num := flag.Int("num", 1, "How many times to print the message?")
 
-	var wg sync.WaitGroup
-	wg.Add(3)
-	go one(&wg)
-	go two(&wg)
-	go three(&wg)
+	// Print all in Caps:
+	caps := flag.Bool("caps", false, "should the string be in all uppercase?")
+	flag.Parse()
+	// Print in all Caps:
 
-	// let's delay
-	wg.Wait() //Wait means: wait until wg holds the value 0.
+	// Make the message print in uppercase:
+
+	if *caps {
+		for i := 0; i < *num; i++ {
+			fmt.Println(strings.ToUpper(*msg))
+		}
+	} else if !(*caps) {
+		for i := 0; i < *num; i++ {
+			fmt.Println((*msg))
+		}
+	}
 }
